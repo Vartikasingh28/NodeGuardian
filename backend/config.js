d3e77ef7fe91c9ca79ab164c8875a9d82fde3884
Vartikasingh28 <vartikasingh2810@gmail.com>
2023-05-20 13:33:01 +0530

    first commit

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+
+const api_config = {
+    PORT : 5000,
+    DB_URL : 'mongodb+srv://vartikasingh28:vartika28@cluster0.mjcbyat.mongodb.net/node_guardian?retryWrites=true&w=majority'
+}
+
+module.exports = api_config;
---
+++ frontend/src/App.js
@@ -0,0 +1,56 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Admin from "./components/admin";
+import Main from "./components/main";
+import Login from "./components/main/Login";
+import Register from "./components/main/Register";
+import User from "./components/user";
+import CodeGenerator from "./components/user/CodeGenerator";
+import CodeBrowser from "./components/user/CodeBrowser";
+import Home from "./components/main/Home";
+import AdminProvider from "./context/AdminProvider";
+import UserProvider from "./context/UserProvider";
+import UserProfile from "./components/user/UserProfile";
+import AdminAuth from "./auth/AdminAuth";
+import UserAuth from "./auth/UserAuth";
+import { useState } from "react";
+import {Toaster} from 'react-hot-toast';
+import MyTemplates from "./components/user/MyTemplates";
+
+function App() {
+
+  const [currentUser, setCurrentUser] = useState(JSON.parse(sessionStorage.getItem('user')));
+  const [currentAdmin, setCurrentAdmin] = useState(JSON.parse(sessionStorage.getItem('admin')));
+
+  return (
+    <div>
+      <BrowserRouter>
+      <Toaster position="top-center" />
+        <AdminProvider currentAdmin={currentAdmin}>
+          <UserProvider currentUser={currentUser}>
+            <Routes>
+              <Route path="/" element={<Navigate to="/main/home" />} />
+              <Route path="admin" element={<AdminAuth><Admin /></AdminAuth>}>
+                {/* <Route path='managegames' element={<ManageGames />}  /> */}
+              </Route>
+              <Route path="user" element={<UserAuth><User /></UserAuth>}>
+                <Route path="generator" element={<CodeGenerator />} />
+                <Route path="browser" element={<CodeBrowser />} />
+                <Route path="profile" element={<UserProfile />} />
+                <Route path="mytemplates" element={<MyTemplates />} />
+              </Route>
+              <Route path="main" element={<Main />}>
+                <Route path="login" element={<Login />} />
+                <Route path="register" element={<Register />} />
+                <Route path="home" element={<Home />} />
+              </Route>
+            </Routes>
+          </UserProvider>
+        </AdminProvider>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
